563acd05b70466e58df9be6c2ef0fe7d78ebcce8
repetitioestmaterstudiorum <thotan@ik.me>
2021-09-18 23:43:23 +0200

    delete deck security, context notes

--- imports/api/methods/cardMethods.js
+++ imports/api/methods/cardMethods.js
@@ -7,5 +7,7 @@ import { CardCollection } from '/imports/api/collections/cardCollection.js'
 // Meteor.methods({})
 
 export function deleteDecksCards(deckId) {
+	if (!deckId) throw new Meteor.Error('No deckId to delete cards of.')
+
 	CardCollection.update({ deckId }, { $set: { deleted: true } }, { multi: true })
 }
--- imports/api/methods/deckMethods.js
+++ imports/api/methods/deckMethods.js
@@ -9,7 +9,15 @@ import { deleteDecksCards } from '/imports/api/methods/cardMethods.js'
 Meteor.methods({
 	deleteDeck(deckId) {
 		check(deckId, String)
+
 		if (!this.userId) throw new Meteor.Error('Not authorized.')
+
+		const isUsersDeck = !!DeckCollection.findOne(
+			{ _id: deckId, userId: this.userId },
+			{ fields: { _id: 1 } }
+		)
+		if (!isUsersDeck) throw new Meteor.Error('Access denied.')
+
 		DeckCollection.update({ _id: deckId }, { $set: { deleted: true } })
 		deleteDecksCards(deckId)
 	},
